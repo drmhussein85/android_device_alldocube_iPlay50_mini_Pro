77db55de3a779ca0bcf6601ce0ca9f76b7f0160d
drmhussein85 <66475390+drmhussein85@users.noreply.github.com>
2023-12-26 17:46:50 +0300

    Update omni_iPlay50_mini_Pro.mk
    change omni to twrp

--- omni_iPlay50_mini_Pro.mk
+++ omni_iPlay50_mini_Pro.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from iPlay50_mini_Pro device
 $(call inherit-product, device/alldocube/iPlay50_mini_Pro/device.mk)
 
 PRODUCT_DEVICE := iPlay50_mini_Pro
-PRODUCT_NAME := omni_iPlay50_mini_Pro
+PRODUCT_NAME := twrp_iPlay50_mini_Pro
 PRODUCT_BRAND := Alldocube
 PRODUCT_MODEL := iPlay50_mini_Pro
 PRODUCT_MANUFACTURER := alldocube
