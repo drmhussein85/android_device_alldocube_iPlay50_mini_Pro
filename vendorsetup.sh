8b6ee47d304a70d0c50331f6f06a470fd8c6595d
drmhussein85 <66475390+drmhussein85@users.noreply.github.com>
2023-12-26 17:45:19 +0300

    Update vendorsetup.sh
    change omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_iPlay50_mini_Pro-user
-add_lunch_combo omni_iPlay50_mini_Pro-userdebug
-add_lunch_combo omni_iPlay50_mini_Pro-eng
+add_lunch_combo twrp_iPlay50_mini_Pro-user
+add_lunch_combo twrp_iPlay50_mini_Pro-userdebug
+add_lunch_combo twrp_iPlay50_mini_Pro-eng
